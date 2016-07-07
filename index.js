a24eb9816d656a2d78fcb1578f38412df4f3dc83
Rafal Wilinski <raf.wilinski@gmail.com>
2016-07-07 10:31:51 +0200

    Added drop-files event support

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ module.exports = function create (opts) {
     menubar.tray = opts.tray || new Tray(iconPath)
     menubar.tray.on(defaultClickEvent, clicked)
     menubar.tray.on('double-click', clicked)
+    menubar.tray.on('drop-files', dropped);
     menubar.tray.setToolTip(opts.tooltip)
 
     if (opts.preloadWindow || opts['preload-window']) {
@@ -61,6 +62,10 @@ module.exports = function create (opts) {
     menubar.showWindow = showWindow
     menubar.hideWindow = hideWindow
     menubar.emit('ready')
+
+    function dropped (e, files) {
+      menubar.emit('drop-files', files)
+    }
 
     function clicked (e, bounds) {
       if (e.altKey || e.shiftKey || e.ctrlKey || e.metaKey) return hideWindow()
