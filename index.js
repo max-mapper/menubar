977526535b709a34e6158f13bccde0bf8d2c05f5
Kyle Ramey <CodeFusion@users.noreply.github.com>
2016-10-12 13:07:58 -0400

    Support creation of menubar after app init
    Checks if app.isReady, allowing menubar to be added at any point during runtime.

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ module.exports = function create (opts) {
   opts.height = opts.height || 400
   opts.tooltip = opts.tooltip || ''
 
-  app.on('ready', appReady)
+  if(app.isReady()) appReady()
+  else app.on('ready', appReady)
 
   var menubar = new events.EventEmitter()
   menubar.app = app
