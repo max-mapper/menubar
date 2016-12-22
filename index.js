9f243881edd16b2aae1ae7a5c517bfcd1cbb86ba
rhysd <lin90162@yahoo.co.jp>
2016-12-22 11:57:43 +0900

    Fix window does not show when already app is ready
    `appReady()` is not considered to be called in `create()`. So calling
    it directly in `appReady()` does not work. #145 does not work.
    My PR (#144) was care about it but it was closed.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ module.exports = function create (opts) {
   opts.height = opts.height || 400
   opts.tooltip = opts.tooltip || ''
 
-  if (app.isReady()) appReady()
+  if (app.isReady()) process.nextTick(appReady)
   else app.on('ready', appReady)
 
   var menubar = new events.EventEmitter()
