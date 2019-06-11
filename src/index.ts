d42ecdd2200c6cda5460265a671ee3ad0c6ece61
Amaury Martiny <amaury.martiny@protonmail.com>
2019-06-12 00:51:57 +0200

    Fix export

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { app } from 'electron';
 import { Menubar } from './Menubar';
 import { Options } from './types';
 
-export * from './util/cleanOptions';
+export * from './util/getWindowPosition';
 export { Menubar };
 
 export function menubar (options?: Partial<Options> | string) {
