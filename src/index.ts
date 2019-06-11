0087979ede42761c11f13c7cf53bae873ebd8b12
Amaury Martiny <amaury.martiny@protonmail.com>
2019-06-12 00:49:25 +0200

    Export taskbar position

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { app } from 'electron';
 import { Menubar } from './Menubar';
 import { Options } from './types';
 
+export * from './util/cleanOptions';
 export { Menubar };
 
 export function menubar (options?: Partial<Options> | string) {
